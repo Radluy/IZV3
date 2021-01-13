3be1e4961abda5db60a1a00f15bbcadf6a929ec6
Radoslav Elias <radoslave0@gmail.com>
2021-01-13 15:23:44 +0100

    first plot

--- geo.py
+++ geo.py
@@ -3,20 +3,41 @@
 import pandas as pd
 import geopandas
 import matplotlib.pyplot as plt
-import contextily
+import contextily as ctx
 import sklearn.cluster
 import numpy as np
+import cartopy.crs as ccrs
 # muzeze pridat vlastni knihovny
 
 
 
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
+    df = df.loc[df['region'] == 'JHM']
+    gdf = geopandas.GeoDataFrame(
+        df, geometry=geopandas.points_from_xy(df["d"], df["e"]), crs="EPSG:5514"
+    )
+    gdf = gdf.to_crs("EPSG:3857")
+    return gdf
 
 
 def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
              show_figure: bool = False):
     """ Vykresleni grafu s dvemi podgrafy podle lokality nehody """
+    fig,ax=plt.subplots(1,2,figsize=(10,8))
+    gdf[gdf["p5a"]==1].plot(ax=ax[0], markersize=2, label="V obci")
+    ctx.add_basemap(ax[0],crs=gdf.crs.to_string(),
+                    source=ctx.providers.Stamen.TonerLite,alpha=0.9)
+    
+    gdf[gdf["p5a"]==2].plot(ax=ax[1], markersize=2, color="r")
+    ctx.add_basemap(ax[1],crs=gdf.crs.to_string(),
+                    source=ctx.providers.Stamen.TonerLite,alpha=0.9)
+
+    if (fig_location):
+        plt.savefig(fig_location)
+
+    if (show_figure):
+        plt.show()
 
 
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
