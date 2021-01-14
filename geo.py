84c59e00d0de3fdcdac2cbc1e22c54cbbac12444
Radoslav Elias <radoslave0@gmail.com>
2021-01-14 10:38:50 +0100

    geo logic done

--- geo.py
+++ geo.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 import contextily as ctx
 import sklearn.cluster
 import numpy as np
-import cartopy.crs as ccrs
-# muzeze pridat vlastni knihovny
+
 
 
 
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
-    """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
+
     df = df.loc[df['region'] == 'JHM']
+    df.dropna(axis='rows', inplace=True)
     gdf = geopandas.GeoDataFrame(
         df, geometry=geopandas.points_from_xy(df["d"], df["e"]), crs="EPSG:5514"
     )
@@ -23,8 +23,8 @@ def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
 
 def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
              show_figure: bool = False):
-    """ Vykresleni grafu s dvemi podgrafy podle lokality nehody """
-    fig,ax=plt.subplots(1,2,figsize=(12,11))
+
+    fig,ax=plt.subplots(1,2,figsize=(20,18))
     gdf[gdf["p5a"]==1].plot(ax=ax[0], markersize=1).set_title("Nehody v JHM kraji: V obci")
     ctx.add_basemap(ax[0],crs=gdf.crs.to_string(),
                     source=ctx.providers.Stamen.TonerLite,zoom=10, alpha=0.9)
@@ -45,9 +45,30 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
 
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
-    """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
 
+    coords = np.dstack([gdf.geometry.x, gdf.geometry.y]).reshape(-1, 2)
+    db = sklearn.cluster.MiniBatchKMeans(n_clusters=12).fit(coords)
 
+    gdf1 = gdf.copy()
+    gdf1["cluster"] = db.labels_
+
+    gdf2 = gdf1.dissolve(by="cluster", aggfunc={"p2a": "count"}).rename(columns=dict(p2a="cnt"))
+
+    gdf_coords = geopandas.GeoDataFrame(geometry=geopandas.points_from_xy(db.cluster_centers_[:, 0], db.cluster_centers_[:, 1]))
+    gdf3 = gdf2.merge(gdf_coords, left_on="cluster", right_index=True).set_geometry("geometry_y")
+
+    plt.figure(figsize=(20, 20))
+    ax = plt.gca()
+    gdf3.plot(ax=ax, markersize=gdf3["cnt"] / 10, column="cnt", legend=True, alpha=0.5)
+    ctx.add_basemap(ax, crs="epsg:3857", source=ctx.providers.Stamen.TonerLite, alpha=0.6)
+
+    ax.axis("off")
+
+    if (fig_location):
+        plt.savefig(fig_location)
+
+    if (show_figure):
+        plt.show()
 
 if __name__ == "__main__":
     # zde muzete delat libovolne modifikace
