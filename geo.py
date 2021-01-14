edbd956b9520335cb0d8518d02d01bbd273421d1
Radoslav Elias <radoslave0@gmail.com>
2021-01-14 13:23:27 +0100

    pep formatting

--- geo.py
+++ geo.py
@@ -7,6 +7,7 @@ import contextily as ctx
 import sklearn.cluster
 import numpy as np
 
+
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     '''
     Plots the types of surfaces when car crashes happened.
@@ -20,7 +21,8 @@ def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     df = df.loc[df['region'] == 'JHM']
     df.dropna(axis='rows', inplace=True)
     gdf = geopandas.GeoDataFrame(
-        df, geometry=geopandas.points_from_xy(df["d"], df["e"]), crs="EPSG:5514"
+        df, geometry=geopandas.points_from_xy(df["d"], df["e"]),
+        crs="EPSG:5514"
     )
     gdf = gdf.to_crs("EPSG:3857")
     return gdf
@@ -33,19 +35,26 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     happened in a village or not.
 
             Parameters:
-                    gdf (geopandas.GeoDataFrame): GeoDataFrame with data to plot
+                    gdf (geopandas.GeoDataFrame): GeoDataFrame with
+                    data to plot
                     fig_location (str): File name to save figure
                     show_figure (bool): Whether to show figure
 
     '''
-    fig,ax=plt.subplots(1,2,figsize=(20,18))
-    gdf[gdf["p5a"]==1].plot(ax=ax[0], markersize=1).set_title("Nehody v JHM kraji: V obci")
-    ctx.add_basemap(ax[0],crs=gdf.crs.to_string(),
-                    source=ctx.providers.Stamen.TonerLite,zoom=10, alpha=0.9)
+    fig, ax = plt.subplots(1, 2, figsize=(20, 18))
+    gdf[gdf["p5a"] == 1].plot(ax=ax[0],
+                              markersize=1).set_title(
+                                  "Nehody v JHM kraji: V obci")
+    ctx.add_basemap(ax[0], crs=gdf.crs.to_string(),
+                    source=ctx.providers.Stamen.TonerLite,
+                    zoom=10, alpha=0.9)
 
-    gdf[gdf["p5a"]==2].plot(ax=ax[1], markersize=1, color="r").set_title("Nehody v JHM kraji: Mimo obce")
-    ctx.add_basemap(ax[1],crs=gdf.crs.to_string(),
-                    source=ctx.providers.Stamen.TonerLite,alpha=0.9, zoom=10)
+    gdf[gdf["p5a"] == 2].plot(ax=ax[1], markersize=1,
+                              color="r").set_title(
+                    "Nehody v JHM kraji: Mimo obce")
+    ctx.add_basemap(ax[1], crs=gdf.crs.to_string(),
+                    source=ctx.providers.Stamen.TonerLite,
+                    alpha=0.9, zoom=10)
 
     ax[0].axis("off")
     ax[1].axis("off")
@@ -63,7 +72,8 @@ def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     Plots the accidents aggregated into 12 clusters onto a map.
 
             Parameters:
-                    gdf (geopandas.GeoDataFrame): GeoDataFrame with data to plot
+                    gdf (geopandas.GeoDataFrame): GeoDataFrame with
+                    data to plot
                     fig_location (str): File name to save figure
                     show_figure (bool): Whether to show figure
 
@@ -74,15 +84,22 @@ def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
     gdf1 = gdf.copy()
     gdf1["cluster"] = db.labels_
 
-    gdf2 = gdf1.dissolve(by="cluster", aggfunc={"p2a": "count"}).rename(columns=dict(p2a="cnt"))
+    gdf2 = gdf1.dissolve(by="cluster",
+                         aggfunc={"p2a": "count"}).rename(
+                            columns=dict(p2a="cnt"))
 
-    gdf_coords = geopandas.GeoDataFrame(geometry=geopandas.points_from_xy(db.cluster_centers_[:, 0], db.cluster_centers_[:, 1]))
-    gdf3 = gdf2.merge(gdf_coords, left_on="cluster", right_index=True).set_geometry("geometry_y")
+    gdf_coords = geopandas.GeoDataFrame(
+        geometry=geopandas.points_from_xy(db.cluster_centers_[:, 0],
+                                          db.cluster_centers_[:, 1]))
+    gdf3 = gdf2.merge(gdf_coords, left_on="cluster",
+                      right_index=True).set_geometry("geometry_y")
 
     plt.figure(figsize=(20, 20))
     ax = plt.gca()
-    gdf3.plot(ax=ax, markersize=gdf3["cnt"] / 10, column="cnt", legend=True, alpha=0.5)
-    ctx.add_basemap(ax, crs="epsg:3857", source=ctx.providers.Stamen.TonerLite, alpha=0.6)
+    gdf3.plot(ax=ax, markersize=gdf3["cnt"] / 10, column="cnt",
+              legend=True, alpha=0.5)
+    ctx.add_basemap(ax, crs="epsg:3857",
+                    source=ctx.providers.Stamen.TonerLite, alpha=0.6)
 
     ax.axis("off")
 
@@ -96,4 +113,3 @@ if __name__ == "__main__":
     gdf = make_geo(pd.read_pickle("accidents.pkl.gz"))
     plot_geo(gdf, "geo1.png", True)
     plot_cluster(gdf, "geo2.png", True)
-
