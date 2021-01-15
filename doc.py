229a68da4de155732a54cc0105006f32b71b1de0
Radoslav Elias <radoslave0@gmail.com>
2021-01-15 13:09:30 +0100

    hypothesis notebook

--- doc.py
+++ doc.py
@@ -2,6 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+df = pd.read_pickle("accidents.pkl.gz")
+print(df)
 
+df_m = pd.DataFrame()
+df_m['vehicle_type'] = df.p44
+df_m['village'] = df.p5a
+print(df_m)
 
-p44 == 13
+plt.figure(figsize=(20, 20))
+ax = plt.gca()
+df_m.plot(ax=ax)
+plt.show()
+#p44 == 13  //cyclist
+#p5a == 1   //in village
+#p5a == 2   //outside village
